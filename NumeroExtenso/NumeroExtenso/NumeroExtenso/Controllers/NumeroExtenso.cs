a9606c7fa6eaf95107ae85e30df93f013f596bb1
ThiWB <thibalbinot@gmail.com>
2025-03-21 14:02:46 -0300

    NumeroExtenso

---
+++ NumeroExtenso/NumeroExtenso/NumeroExtenso/Controllers/NumeroExtenso.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NumeroExtenso.Models;
+
+namespace NumeroExtenso.Controllers
+{
+    public class NumeroExtensoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult NumeroExtenso(int number)
+        {
+            var model = new NumberConversion { Number = number };
+            var extenso = model.ToExtenso();
+            ViewData["Extenso"] = extenso;
+
+            return View();
+        }
+    }
+}
+
