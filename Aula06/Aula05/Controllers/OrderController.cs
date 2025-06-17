8e181afb8af9a058e801385da96c37e83787f473
Thiago Wurster Balbinot <255285@unoesc.lan>
2025-06-16 21:56:54 -0300

    Aula06

---
+++ Aula06/Aula05/Controllers/OrderController.cs
@@ -0,0 +1,57 @@
+﻿using Aula05.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Modelo;
+using Repository;
+
+namespace Aula05.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IWebHostEnvironment environment;
+
+        private readonly OrderRepository _orderRepository;
+
+        private readonly CustomerRepository _customerRepository;
+
+        private readonly ProductRepository _productRepository;
+
+        public object OrderItem { get; private set; }
+
+        public OrderController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+            _orderRepository = new OrderRepository();
+            _customerRepository = new CustomerRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_orderRepository.RetrieveAll());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            OrderViewModel viewModel = new();
+            viewModel.Customers = _customerRepository.RetrieveAll();
+
+            var products = _productRepository.RetrieveAll();
+            List<SelectedItem> items = new List<SelectedItem>();
+
+            foreach (var item in products)
+            {
+                items.Add(new SelectedItem()
+                {
+                    OrderItem = new()
+                    {
+                        Product = (Product)products
+                    }
+                });
+            }
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Aula06/Aula05/ViewModels/OrderViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Modelo;
+
+namespace Aula05.ViewModels
+{
+    public class OrderViewModel
+    {
+        public List<Customer> Customers { get; set; } = [];
+
+        public int CustomerId {  get; set; }
+
+        public Customer? Customer{ get; set; }
+
+        public List<SelectedItem>? SelectedItems { get; set; }
+        public IEnumerable<object> Products { get; internal set; }
+        public List<SelectedItem> Items { get; internal set; }
+    }
+
+    public class SelectedItem
+    {
+        public bool IsSelected { get; set; } = false;
+        public OrderItem OrderItem { get; set; } = null!;
+    }
+}
