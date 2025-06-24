afc3205391cc9cab5b60cdd0754bda5d4c35b389
Thiago Wurster Balbinot <255285@unoesc.lan>
2025-06-23 21:59:45 -0300

    Imoveis

---
+++ CorretorImoveis/CorretorImoveis/Controllers/ImoveisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorretorImoveis.Controllers
+{
+    public class ImoveisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
